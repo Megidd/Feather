72977d369c7215a6acdd3f49402e845dd3590700
Megidd <megiddgit@gmail.com>
2023-08-21 11:13:49 +0330

    Get required parameters from user

--- RhinoCommon/Feather/Feather/FeatherHollow.cs
+++ RhinoCommon/Feather/Feather/FeatherHollow.cs
@@ -25,6 +25,22 @@ namespace Feather
                 return Result.Failure;
             }
 
+            float thickness = Helper.GetFloatFromUser(1.8, 0.0, 100.0, "Enter wall thickness for hollowing.");
+
+            uint precision = Helper.GetUint32FromUser("Enter precision: Low=1, Medium=2, High=3", 2, 1, 3);
+            switch (precision)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                default:
+                    RhinoApp.WriteLine("Precision must be 1, 2, or 3 i.e. Low=1, Medium=2, High=3");
+                    return Result.Failure;
+            }
+
+            bool infill = Helper.GetYesNoFromUser("Do you want infill for hollowed mesh?");
+
             return Result.Success;
         }
     }
--- RhinoCommon/Feather/Feather/Helper.cs
+++ RhinoCommon/Feather/Feather/Helper.cs
@@ -2,6 +2,7 @@
 using Rhino.Commands;
 using Rhino.DocObjects;
 using Rhino.Geometry;
+using Rhino.Input;
 using Rhino.Input.Custom;
 using System;
 using System.Collections.Generic;
@@ -156,6 +157,75 @@ namespace Feather
                     byte[] attribute = new byte[2];
                     fileStream.Write(attribute, 0, attribute.Length);
                 }
+            }
+        }
+
+
+        public static float GetFloatFromUser(double defaultValue, double lowerLimit, double upperLimit, string message)
+        {
+            // Create a GetNumber object
+            GetNumber numberGetter = new GetNumber();
+            numberGetter.SetLowerLimit(lowerLimit, false);
+            numberGetter.SetUpperLimit(upperLimit, false);
+            numberGetter.SetDefaultNumber(defaultValue);
+            numberGetter.SetCommandPrompt(message);
+
+            // Prompt the user to enter a number
+            GetResult result = numberGetter.Get();
+
+            // Check if the user entered a number
+            switch (result)
+            {
+                case GetResult.Number:
+                    break;
+                default:
+                    return Convert.ToSingle(defaultValue);
+            }
+
+            // Get the number entered by the user
+            double number = numberGetter.Number();
+
+            return Convert.ToSingle(number);
+        }
+
+
+        public static uint GetUint32FromUser(string prompt, uint defaultValue, uint lowerLimit, uint upperLimit)
+        {
+            double doubleResult = defaultValue;
+            uint result = defaultValue;
+            while (true)
+            {
+                var getNumberResult = RhinoGet.GetNumber(prompt, false, ref doubleResult, lowerLimit, upperLimit);
+                if (getNumberResult == Result.Cancel)
+                {
+                    RhinoApp.WriteLine("Canceled by user.");
+                    return defaultValue;
+                }
+                else if (getNumberResult == Result.Success)
+                {
+                    result = (uint)doubleResult;
+                    if (result < lowerLimit || result > upperLimit)
+                        RhinoApp.WriteLine("Input out of range.");
+                    else
+                        return result;
+                }
+                else
+                    RhinoApp.WriteLine("Invalid input.");
+            }
+        }
+
+        public static bool GetYesNoFromUser(string prompt)
+        {
+            bool boolResult = false;
+            while (true)
+            {
+                var getBoolResult = RhinoGet.GetBool(prompt, true, "No", "Yes", ref boolResult);
+                if (getBoolResult == Result.Cancel)
+                    RhinoApp.WriteLine("Canceled by user.");
+                else if (getBoolResult == Result.Success)
+                    return boolResult;
+                else
+                    RhinoApp.WriteLine("Invalid input.");
             }
         }
     }
