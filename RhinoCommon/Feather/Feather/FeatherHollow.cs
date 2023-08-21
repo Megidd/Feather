c82b8efe5337fa6e420104606f7df9dc84ce7844
Megidd <megiddgit@gmail.com>
2023-08-21 10:47:40 +0330

    New command

---
+++ RhinoCommon/Feather/Feather/FeatherHollow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Feather
+{
+    public class FeatherHollow : Command
+    {
+        public FeatherHollow()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static FeatherHollow Instance { get; private set; }
+
+        public override string EnglishName => "FeatherHollow";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            string inputStl = "input.stl";
+
+            if (Helper.GetInputStl(inputStl) == Result.Failure)
+            {
+                return Result.Failure;
+            }
+
+            return Result.Success;
+        }
+    }
+}
