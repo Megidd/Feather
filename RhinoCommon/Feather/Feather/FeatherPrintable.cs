0153012fc83fdafeacfabd20a1039bdaabcc9451
Megidd <megiddgit@gmail.com>
2023-09-02 13:42:46 +0330

    New command. Printability

---
+++ RhinoCommon/Feather/Feather/FeatherPrintable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Feather
+{
+    public class FeatherPrintable : Command
+    {
+        public FeatherPrintable()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of the MyCommand command.</summary>
+        public static FeatherPrintable Instance { get; private set; }
+
+        public override string EnglishName => "FeatherPrintable";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
